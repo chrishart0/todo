19c49316ac66f986b1aa17e1e53fe2cc1c956d3e
chrishart0 <christianphart0@gmail.com>
2023-02-02 17:40:32 -0500

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import {Amplify} from "aws-amplify"
+import awsconfig from "../aws-exports"
+import '@aws-amplify/ui-react/styles.css'
 
-export default function App({ Component, pageProps }) {
+Amplify.configure({...awsconfig,ssr:true})
+
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default MyApp
