353a9b0c2e242578e742242e24a753121ec20cd8
chrishart0 <christianphart0@gmail.com>
2023-02-03 15:49:18 -0500

    add material ui

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,36 @@
+
+import * as React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../config/theme';
+import createEmotionCache from '../config/createEmotionCache';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
 import '../styles/globals.css'
-import {Amplify} from "aws-amplify"
+import { Amplify } from "aws-amplify"
 import awsconfig from "../aws-exports"
 import '@aws-amplify/ui-react/styles.css'
 
-Amplify.configure({...awsconfig,ssr:true})
+Amplify.configure({ ...awsconfig, ssr: true })
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp( props ) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
